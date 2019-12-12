94a1d9ec9e6be936ea14c5cf3b36a48b2f256379
root <root@debian>
2019-12-12 09:10:20 +0100

    Initial commit

---
+++ run.php
@@ -0,0 +1,61 @@
+<?php
+
+define('LOCK_FILE', "/var/run/" . basename($argv[0], ".php") . ".lock");
+
+if (!tryLock())
+    die("Already running.\n");
+
+# remove the lock on exit (Control+C doesn't count as 'exit'?)
+register_shutdown_function('unlink', LOCK_FILE);
+
+# The rest of your script goes here....
+$pid = getmypid();
+echo "Hello world! Your PID is: ".$pid."\n";
+file_put_contents('pid.txt', $pid);
+
+require __DIR__.'/../composer-yasmin/vendor/autoload.php';
+
+// Include composer autoloader
+
+$loop = \React\EventLoop\Factory::create();
+$client = new \CharlotteDunois\Yasmin\Client(array(), $loop);
+
+$client->on('error', function ($error) {
+    echo $error.PHP_EOL;
+});
+
+$client->on('ready', function () use ($client) {
+    echo 'Logged in as '.$client->user->tag.' created on '.$client->user->createdAt->format('d.m.Y H:i:s').PHP_EOL;
+});
+
+$client->on('message', function ($message) {
+    echo 'Received Message from '.$message->author->tag.' in '.($message->channel instanceof \CharlotteDunois\Yasmin\Interfaces\DMChannelInterface ? 'DM' : 'channel #'.$message->channel->name ).' with '.$message->attachments->count().' attachment(s) and '.\count($message->embeds).' embed(s)'.PHP_EOL;
+});
+
+$client->login($_ENV['SMF_TOKEN'])->done();
+$loop->run();
+
+sleep(30);
+
+exit(0);
+
+function tryLock()
+{
+    # If lock file exists, check if stale.  If exists and is not stale, return TRUE
+    # Else, create lock file and return FALSE.
+
+    if (@symlink("/proc/" . getmypid(), LOCK_FILE) !== FALSE) # the @ in front of 'symlink' is to suppress the NOTICE you get if the LOCK_FILE exists
+        return true;
+
+    # link already exists
+    # check if it's stale
+    if (is_link(LOCK_FILE) && !is_dir(LOCK_FILE))
+    {
+        unlink(LOCK_FILE);
+        # try to lock again
+        return tryLock();
+    }
+
+    return false;
+}
+?>
