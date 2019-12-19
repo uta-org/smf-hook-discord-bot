9110bccfacc7fdf063e0e448881109e031e73738
Alumno72 <Alumno72@Formacion.com>
2019-12-19 11:05:18 +0100

    Fixbug: Using $table_name param

--- funcs/db.funcs.php
+++ funcs/db.funcs.php
@@ -1,7 +1,7 @@
 <?php
 
 function getResult($db, $table_name, $condition, $data, $columns = '*') {
-	$stmt = $db->prepare('SELECT '.$columns.' FROM '.$condition.' WHERE '.$condition);
+	$stmt = $db->prepare('SELECT '.$columns.' FROM '.$table_name.' WHERE '.$condition);
 	// $stmt->bindParam(1, $user_id, PDO::PARAM_STR);
 	$stmt->execute($data);
 	$row = $stmt->fetch(PDO::FETCH_ASSOC);
