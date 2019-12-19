de4b95657f13a290b400d9bdf4238e5f9829cf07
Alumno72 <Alumno72@Formacion.com>
2019-12-19 13:24:24 +0100

    Adding test new

--- funcs/run.funcs.php
+++ funcs/run.funcs.php
@@ -1,5 +1,7 @@
 <?php
 
+use PHPHtmlParser\Dom;
+
 function startListening($db, $client, $message, $params) {
     try {
     	if(null === @$params[0]) {
@@ -76,7 +78,7 @@ function getChannelById($client, $id) {
 
 function getAllChannelIds($client) {
     // TODO: Use PHP Linq
-    
+
 	$ids = array();
 
     foreach ($client->channels->all() as $channel) 
@@ -95,11 +97,13 @@ function runCrawler($db, $client, $instance_id) {
 	$smf_url = $instance_data['smf_url'];
 	$board_id = $instance_data['board_id'];
 
-	$crawl_url = appendSlash($smf_url).'-b'.$board_id.'.0';
+    // https://foro.elhacker.net/SSI.php?ssi_function=boardNews;board=34;start=1;limit=5;length=500
+    // $crawl_url = appendSlash($smf_url).'-b'.$board_id.'.0';
+    $crawl_url = appendSlash($smf_url).'SSI.php?ssi_function=boardNews;board='.$board_id.';start=1;limit=5;length=500';
 	$channel = getChannelById($client, $instance_data['channel_id']);
 
 	// TODO: Create infinite loop to crawl data, but first create an example where the last topic is output
-    runLoop($crawl_url, $channel);	
+    runLoop($crawl_url, $channel);
 }
 
 /*
@@ -113,8 +117,71 @@ $loop->addPeriodicTimer(5 * 60, function () {
 */
 
 function runLoop($url, $channel) {
+    $dom = new Dom;
+    $dom->loadFromUrl($url);
+
+    $divs = $dom->find("div");    
+    $tables = $dom->find("table");
+
+    $data = array();
+
+    if(count($divs) != count($tables)) {
+        $channel->send(':stop_sign: Error ocurred on the server side!');
+        echo 'Div count is not the same of table count! ('.count($divs).' != '.count($tables).')'.PHP_EOL;
+        return;
+    }
+
+    $j = 0;
+    for ($i=0; $i < count($divs); $i++) {
+        // TODO: Get id from link (discord_bot_news), then if id from last new is less than the actual id then continue
+
+        $div = $divs[$i];
+        $table = $tables[$i];
+
+        $title_a = $div->find("a")[0];
+        $table_contents = $table->find("font")[0]->find("b")[0];
+
+        $published_date = $table_contents->text;
+        $published_date .= $table_contents->nextSibling()->text;
+
+        $user_element = $table_contents->nextSibling();
+
+        $data[$j]["title"] = $title_a->text;
+        $data[$j]["url"] = $title_a->getAttribute('href');
+        $data[$j]["description"] = br2nl($div->find("font")[0]->innerHtml);
+        $data[$j]["published_at"] = $published_date;
+        $data[$j]["user_url"] = $user_element->getAttribute("href");
+        $data[$j]["username"] = $user_element->text;
+
+        ++$j;
+    }
+
+    for ($k=0; $k < $j; $k++) { 
+        echo "[".$k."] Publishing new! Data: ".PHP_EOL.print_r($data[$k], true).PHP_EOL.PHP_EOL;
+        $new = ":notepad_spiral: [".$data[$k]["title"]."](".$data[$k]["url"].")".PHP_EOL.PHP_EOL;
+        $new .= $data[$k]["description"].PHP_EOL;
+        $new .= "[Leer más](".$data[$k]["url"].")".PHP_EOL;
+        $new .= "Noticia publicada **".$data[$k]["published_at"]."** por [".$data[$k]["username"]."](".$data[$k]["user_url"].")";
+
+        $channel->send($new);   
+    }
 
 }
+
+/*
+
+:notepad_spiral: [Google Cloud puede decir adiós si no supera en cuota a Amazon Web Services o ...](http://google.com/)
+
+Una de las luchas más interesantes que se están dando den la informática actual está muy lejos del escritorio. Se da en la nube, y enfrenta a tres colosos de la computación, como son Amazon, con Amazon Web Services (AWS), Microsoft, con Azure, y Google con Google Cloud.
+
+El orden en que los hemos mencionado corresponde con el lugar que los tres ocupan en cuota de mercado en el campo de los servidores. La posición actual de Google en el sector no gustaba en el seno de la compañía al...
+
+[Leer más](http://google.com)
+Noticia publicada **Hoy a las 01:00** por [wolfbcn](http://google.com)
+
+===========================================================================
+
+*/
 
 /*
 function getCachedChannels($client) {
--- funcs/string.funcs.php
+++ funcs/string.funcs.php
@@ -54,3 +54,8 @@ function appendSlash($str) {
 
 	return $str.'/';
 }
+
+function nl2br2($string) {
+    $string = str_replace(array("\r\n", "\r", "\n"), "<br />", $string);
+    return $string;
+}
